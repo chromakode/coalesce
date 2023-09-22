d46ad0699c55667e29483074aa633b257a8b2011
Max Goodhart <c@chromakode.com>
2023-09-21 22:53:33 -0700

    Add project user access table

---
+++ project-server/migrations/0003-project-access.ts
@@ -0,0 +1,24 @@
+import { USER_ROLE } from '@shared/constants'
+import { Kysely, sql } from '../deps.ts'
+import { DB } from '../service.ts'
+
+export async function up(db: Kysely<DB>): Promise<void> {
+  await db.schema
+    .createType('user_role')
+    .asEnum(Object.values(USER_ROLE))
+    .execute()
+
+  await db.schema
+    .createTable('project_users')
+    .addColumn('project_id', 'text', (col) =>
+      col.references('project.project_id').onDelete('cascade').notNull(),
+    )
+    .addColumn('user_id', 'text')
+    .addColumn('role', sql`user_role`, (col) => col.notNull())
+    .addPrimaryKeyConstraint('project_users_pk', ['project_id', 'user_id'])
+    .execute()
+}
+
+export async function down(db: Kysely<DB>): Promise<void> {
+  await db.schema.dropTable('project_users').cascade().execute()
+}
