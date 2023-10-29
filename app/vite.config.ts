a3c82e66e34cc62a192f226f3f45bbcac9d77841
Max Goodhart <c@chromakode.com>
2023-10-29 14:15:37 -0700

    More app coalesce-shared fixups
    Digging deeper into the hole of hacking shared dependency resolution via
    aliases and import maps. An import-map will be necessary for sharing
    with Deno, anyway, but I'd hoped npm would install the local package
    sub-dependencies. It looks like this might be possible with npm's
    `install-links=true` option or pnpm, but for now this is the quickest
    fix.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -13,7 +13,9 @@ export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@shared': path.resolve(__dirname, '../shared'),
+      '@shared': path.resolve('./node_modules/coalesce-shared'),
+      // Resolve local tiny-invariant in shared code
+      'tiny-invariant': path.resolve('./node_modules/tiny-invariant'),
       // Fix different lexical instance between shared and app
       lexical: path.resolve('./node_modules/lexical'),
       '@lexical/rich-text': path.resolve('./node_modules/@lexical/rich-text'),
