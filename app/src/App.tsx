57f57b8202ea13f568cb78e924a6da33580f6d02
Max Goodhart <c@chromakode.com>
2023-10-31 23:43:46 -0700

    Add site notice

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,33 @@
+import { useToast } from '@chakra-ui/react'
+import { useEffect } from 'react'
 import { Route, Switch } from 'wouter'
 import { useAPI } from './components/APIContext'
 import { WithSession } from './components/SessionContext'
 import IndexPage from './pages/IndexPage'
 import ProjectPage from './pages/ProjectPage'
 
+const SITE_NOTICE = import.meta.env.VITE_SITE_NOTICE.split('|')
+
 export default function App() {
   const { hasGuestKey } = useAPI()
+
+  const toast = useToast()
+  useEffect(() => {
+    if (SITE_NOTICE) {
+      toast({
+        title: SITE_NOTICE[0],
+        description: SITE_NOTICE[1],
+        position: 'bottom-right',
+        isClosable: true,
+        duration: 30 * 1000,
+        containerStyle: {
+          marginBottom: '9rem',
+          width: '24rem',
+        },
+      })
+    }
+  }, [])
+
   return (
     <Switch>
       <Route path="/home">
